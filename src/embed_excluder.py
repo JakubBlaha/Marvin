c814973ab2533acfd141469698fa081c46be5d35
JakubBlaha <jakub.blaha@gmail.com>
2019-04-10 17:26:18 +0200

    `EmbedExcluder` no more spams the chat

--- src/embed_excluder.py
+++ src/embed_excluder.py
@@ -24,8 +24,6 @@ class EmbedExcluder(Client):
         tag_role = self.get_guild(Config.guild_id).get_role(
             Config.get('embed_exclusion_alert_role_id', None))
 
-        alerted_messages = []  # Used to skip already once alerted messages
-
         while True:
             Logger.info(f'{self}: Running embed exclusion..')
 
@@ -36,20 +34,18 @@ class EmbedExcluder(Client):
                 Logger.info(f'{self}: Checking channel `{channel.name}`')
 
                 async for msg in channel.history():
-                    if not msg.embeds or msg.id in alerted_messages:
+                    if not msg.embeds:
                         continue
 
                     e = msg.embeds[0]
+                    reactions = [r.emoji for r in msg.reactions]
 
-                    if not is_embed_up_to_date(e):
+                    if not ('❌' in reactions or is_embed_up_to_date(e)):
                         await msg.add_reaction('❌')
                         if tag_channel:
                             await tag_channel.send(
                                 f'Outdated embed in channel {channel.mention} *{e.title}... {e.description}*\n{tag_role.mention if tag_role else ""}'
                             )
-
-                        alerted_messages.append(msg.id)
-
                         Logger.info(
                             f'{self}: Excluded `{e.title}... {e.description}...`'
                         )
