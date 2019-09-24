6d1c5a09c2c6f3fa7540fdaf4158128cf13a7f41
JakubBlaha <jakub.blaha@gmail.com>
2019-09-24 17:29:45 +0200

    TimeoutMesssage.send now does not block the current stack.

--- src/timeout_message.py
+++ src/timeout_message.py
@@ -1,23 +1,33 @@
 import asyncio
 
-from discord import NotFound
+from discord import NotFound, Message
 from discord.ext.commands import Context
 
 
 class TimeoutMessage:
     _ctx: Context
     _timeout: int
+    _msg: Message
 
     def __init__(self, ctx: Context, timeout: int = 5):
         self._ctx = ctx
         self._timeout = timeout
 
     async def send(self, *args, **kw):
-        _msg = await self._ctx.send(*args, **kw)
-        await _msg.add_reaction('⏳')
-        await asyncio.sleep(self._timeout)
+        """ Send the message. *args and **kw are the message arguments. """
+        # Send
+        self._msg = await self._ctx.send(*args, **kw)
+        await self._msg.add_reaction('⏳')
+
+        # Schedule deletion
+        asyncio.get_running_loop().create_task(self.delete(self._timeout))
+
+    async def delete(self, delay: int = None):
+        """ Schedule message deletion. """
+        await asyncio.sleep(delay)
+
         try:
-            await _msg.delete()
+            await self._msg.delete()
         except NotFound:
             pass
 
