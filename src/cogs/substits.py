394172a97fdbd7c34d51832c808dc2876e34c296
JakubBlaha <jakub.blaha@gmail.com>
2019-12-04 13:34:39 +0100

    `!substits` now inserts th image into an embed.

--- src/cogs/substits.py
+++ src/cogs/substits.py
@@ -4,7 +4,7 @@ import re
 from dataclasses import dataclass
 from datetime import datetime
 from io import BytesIO
-from typing import Optional, List
+from typing import Optional
 
 import PIL.ImageOps
 import bs4
@@ -25,6 +25,7 @@ from utils import ImageUtils
 BG_COLOR = (35, 39, 42)
 CACHE_KEY = 'substits'
 CACHE_SECONDS = 600
+ATTACHMENT_FILENAME = 'substits.png'
 
 logger = logging.getLogger('Substits')
 
@@ -66,49 +67,16 @@ def get_pdf_data(login_url: str, course_url: str, link_regex: str, username: str
     return PdfScrapeResult(data, filename, True)
 
 
-def date_from_pdf_name(fname: str) -> str:
+def date_from_pdf_name(filename: str) -> str:
     """ Return a date as a str. Example: 010119.ext -> 1. 1. 0019. """
-    _date_enc = os.path.splitext(fname)[0]
+    _date_enc = os.path.splitext(filename)[0]
     day, month, year = _date_enc[:2], _date_enc[2:4], _date_enc[4:]
 
     return f'{day}. {month}. {datetime.now().year // 100}{year}'
 
 
-async def send_image(ctx: Context, img: Image):
-    # Save to a buffer
-    fp = BytesIO()
-    img.save(fp, format='PNG')
-    fp.seek(0)
-
-    # Send image
-    await ctx.send(file=File(fp, filename='substits.png'))
-
-
-def images_to_bytes(images: List[Image]) -> List[bytes]:
-    """ Convert a list of PIL images to a list of byte-strings to optimize caching. """
-    result = []
-
-    for img in images:
-        buffer = BytesIO()
-        img.save(buffer, 'png')
-        result.append(buffer.getvalue())
-
-    return result
-
-
-def images_from_bytes(bytes_: List[bytes]) -> List[Image]:
-    """ Convert a list of bytes-strings to a list of PIL images to restore from cache. """
-    result = []
-
-    for b in bytes_:
-        buffer = BytesIO(b)
-        result.append(PIL.Image.open(buffer))
-
-    return result
-
-
-def process_image(img: Image) -> Image:
-    """ Enhance a copy of the image with a nicer design. """
+def crop_page(img: Image) -> Image:
+    """ Crop a copy of the image as defined in the RemoteConfig. """
     # Get absolute bbox from relative bbox
     w, h = img.size
     rl, rt, rr, rb = RemoteConfig.substits_pdf_bbox  # Relative bbox
@@ -117,9 +85,11 @@ def process_image(img: Image) -> Image:
     # Crop the image with absolute values
     img = img.crop((al, at, ar, ab))
 
-    # Crop to content
-    img = img.crop(img.getbbox())
+    return img
 
+
+def enhance_image(img: Image) -> Image:
+    """ Enhance a copy the image with a dark mode and padding. """
     # Invert the colors
     img = ImageUtils.invert_colors(img)
 
@@ -137,18 +107,14 @@ def process_image(img: Image) -> Image:
 class Substits(Cog, name='Substitutions'):
     bot: Marvin
     _date: str  # User will be informed about the date of the pdf
-    _images: List[Image]
+    _image: Image
 
     def __init__(self, bot: Marvin):
         self.bot = bot
         self._date = 'Yet to load'
-        self._images = []
 
         # Start loop
         self.bot.add_listener(self.on_ready)
-
-        # # Start the plotly orca server
-        # self.bot.loop.run_in_executor(None, go.Figure().to_image)
 
     async def on_ready(self):
         self.reload_data.start()
@@ -162,8 +128,8 @@ class Substits(Cog, name='Substitutions'):
         # Load cached
         cached = Cache.load(CACHE_KEY, CACHE_SECONDS)
         if cached:
-            self._date = cached[0]
-            self._images = images_from_bytes(cached[1])
+            self._date, _bytes = cached
+            self._image = PIL.Image.open(BytesIO(_bytes))
             logger.debug('Loaded cached images')
             return
 
@@ -186,14 +152,35 @@ class Substits(Cog, name='Substitutions'):
 
         # Convert pdf to images
         logger.debug('Converting to images...')
-        self._images = pdf2image.convert_from_bytes(result.data, fmt='png', transparent=True)
-        logger.debug(f'Conversion done: {self._images}')
+        ims = pdf2image.convert_from_bytes(result.data, fmt='png', transparent=True)
+        logger.debug(f'Conversion done: {ims}')
 
-        # Process the images to be nicer
-        self._images = [process_image(img) for img in self._images]
+        # Crop the images as defined in RemoteConfig
+        ims = [crop_page(im) for im in ims]
+
+        # Combine into a one big image
+        widths, heights = zip(*(i.size for i in ims))
+
+        total_width = sum(widths)
+        max_height = max(heights)
+
+        im = PIL.Image.new('RGBA', (total_width, max_height))
+
+        x_offset = 0
+        for _im in ims:
+            im.paste(_im, (x_offset, 0))
+            x_offset += _im.size[0]
+
+        # Crop to content
+        im = im.crop(im.getbbox())
+
+        # Make the image look nicer
+        self._image = enhance_image(im)
 
         # Cache
-        Cache.cache(CACHE_KEY, (self._date, images_to_bytes(self._images)))
+        buffer = BytesIO()
+        self._image.save(buffer, 'PNG')
+        Cache.cache(CACHE_KEY, (self._date, buffer.getvalue()))
 
     @command(aliases=['supl', 'suply', 'sub'])
     @del_invoc
@@ -206,11 +193,17 @@ class Substits(Cog, name='Substitutions'):
 
         await ctx.trigger_typing()
 
-        # Send header
-        await CommandOutput(ctx, title=f'The substitution list for the day **{self._date}**').send(register=False)
+        # Save to a buffer
+        fp = BytesIO()
+        self._image.save(fp, format='PNG')
+        fp.seek(0)
 
-        for img in self._images:
-            await send_image(ctx, img)
+        # Create an embed
+        out = CommandOutput(ctx, title=f'The substitution list for the day **{self._date}**')
+        out.embed.set_image(url=f'attachment://{ATTACHMENT_FILENAME}')
+
+        # Send the embed and the attachment
+        await out.send(register=False, file=File(fp, ATTACHMENT_FILENAME))
 
 
 def setup(bot):
