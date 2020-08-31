fc1d984c2fe73191d8dcfeaea35d690fca6f6b94
jakubblaha <jakub.blaha@gmail.com>
2020-08-31 12:54:41 +0200

    refactor(remote-config): remove command-panel-channel-id

--- src/remote_config.py
+++ src/remote_config.py
@@ -59,7 +59,6 @@ async def config_from_channel(ch: TextChannel, load_dev: bool = False) -> dict:
 # noinspection PyPep8Naming
 class RemoteConfig_(ConfigBase):
     # Optional entries
-    command_panel_channel_id: int = None
     command_panel_timeout: int = 3600
     auto_reactor_channel_ids: List[int] = []
     auto_reactor_reaction_ids: List[int] = []
